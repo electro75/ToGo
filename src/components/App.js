56af6901621a273ad35ca0c716709c08917e7c20
Sahil Naikdhure <sanaikdhure@gmail.com>
2020-04-23 15:55:19 +0530

    adds app header

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 // this will be a smart component (aware of the state)
 // state will have the settings of the current timer
@@ -10,13 +13,36 @@ import './App.css';
 //     return <div>App</div>
 // }
 
-class App extends React.Component {
-    render() {
-        return(
-            <div>App</div>
-        )
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,    
+    },            
+    title: {
+      flexGrow: 1,
+      marginLeft: theme.spacing(2),
+    },
+    bar : {
+        padding : theme.spacing(2)
     }
+}));   
+
+
+
+export default function App() {
+
+    const classes = useStyles();
+                    
+        return(
+            <div className={classes.root} >
+                <AppBar position="static" className={classes.bar} >
+                    <Typography variant="h4" className={classes.title}>
+                        ToGo
+                    </Typography>             
+                </AppBar>
+            </div>
+        )
+    
 }
 
 
-export default App
+// export default App
