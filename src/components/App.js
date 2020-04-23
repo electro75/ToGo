bd69a7e58ce3329d123acea75ca2590e5a1411f8
Sahil Naikdhure <sanaikdhure@gmail.com>
2020-04-23 12:47:09 +0530

    adds css styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import './App.css';
 
-const App = () => {
-    return <div>App</div>
+// this will be a smart component (aware of the state)
+// state will have the settings of the current timer
+// default state will have no timer set.
+
+
+// const App = () => {
+//     return <div>App</div>
+// }
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>App</div>
+        )
+    }
 }
+
 
 export default App
