6767a2fabb811865209bf24921a63532d01ea8cf
Sahil Naikdhure <sanaikdhure@gmail.com>
2020-04-24 13:32:46 +0530

    gets time diff string on screen

--- src/components/timerBody/DisplayTimer.js
+++ src/components/timerBody/DisplayTimer.js
@@ -1,10 +1,70 @@
 import React from 'react';
 
+const intervals = [
+    {interval : 'week', unit : 7 * 24 * 60 * 60},
+    {interval : 'day', unit : 24 * 60 * 60},
+    {interval : 'hour', unit : 60* 60},
+    {interval : 'minute', unit : 60},
+    {interval : 'second', unit : 1}
+]
 
-const DisplayTimer = () => {
-    return (
-        <div>Display Timer</div>
-    )
+
+function getDifference(eventTime) {    
+    let i = 0;
+    let newDiffArr = [
+        { interval : 'week', diff : 0 }, 
+        { interval : 'day', diff : 0 }, 
+        { interval : 'hour', diff : 0 }, 
+        { interval : 'minute', diff : 0 }, 
+        { interval : 'second', diff : 0 }, 
+    ]    
+
+    while(eventTime > 0 && i < 5) {        
+        newDiffArr[i].diff = Math.trunc(eventTime / intervals[i].unit);
+
+        eventTime = eventTime % intervals[i].unit;
+
+        i = i + 1;
+    }    
+
+    return newDiffArr
+}
+
+function getDiffStr(arr) {
+    // console.log(arr);
+    let str = ``;
+    arr.forEach(interval => {
+        // console.log(interval)
+        str = `${str} ` + ((interval.diff > 1 ) ? `${interval.diff} ${interval.interval}s` :
+                (interval.diff === 1) ? `${interval.diff} ${interval.interval}` : ``);                
+    })
+
+    return str;
+}
+
+const DisplayTimer = (props) => {
+
+    if(!props.eventTime) {
+        return (
+            <div>No Event Added!</div>
+        )
+    } else {        
+        let diffStr = '';
+        let d1 = props.currentTime
+        let diff = (props.eventTime) - d1        
+        diffStr = getDiffStr(getDifference(diff));
+
+        setInterval(function() {
+            props.incTime();
+        }, 1000)            
+
+        return (
+            <div>{diffStr}</div>
+        )
+        
+    }
+
+    
 }
 
 export default DisplayTimer;
--- src/components/timerBody/TimerBody.js
+++ src/components/timerBody/TimerBody.js
@@ -4,14 +4,32 @@ import DisplayTimer from './DisplayTimer';
 import './TimerBody.css'
 
 class TimerBody extends React.Component {
+
     constructor(props) {
         super(props);
+
+        this.state = {
+            eventTime : 1588809600,
+            eventName : '25th Bday.', 
+            currentTime : new Date().getTime() / 1000                       
+        }        
     }
+
+    incCurrentTime = () => {        
+        this.setState({            
+            currentTime: new Date().getTime() / 1000
+        })
+    }
+    
 
     render() {
         return (
             <div className="timer-body" >
-                <DisplayTimer />
+                <DisplayTimer  
+                    eventTime = {this.state.eventTime} 
+                    eventName = {this.state.eventName}
+                    currentTime = {this.state.currentTime}
+                    incTime = {this.incCurrentTime}/>
                 <AddTimer />
             </div>
         )
